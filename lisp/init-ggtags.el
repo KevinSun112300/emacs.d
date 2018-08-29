6da0622b3fdbfaaed192c9e3f73869f1472bda86
sunkehui <skh7518671@gmail.com>
2018-08-29 16:19:49 +0800

    delete not needed and add ggtags mode

---
+++ lisp/init-ggtags.el
@@ -0,0 +1,10 @@
+;;TODO init ggtags mode
+(when (maybe-require-package 'ggtags)
+  (add-hook 'c-mode-common-hook
+            (lambda ()
+              (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'php-mode)
+                (ggtags-mode 1))))
+
+
+  )
+(provide 'init-ggtags)
