3818a501eefe0f4479e76eeb1409a9966fc39070
sunkehui <skh7518671@gmail.com>
2018-12-11 11:48:20 +0800

    add init-my

---
+++ lisp/init-my.el
@@ -0,0 +1,11 @@
+;;TODO init ggtags mode
+
+
+
+(setenv "GTAGSLIBPATH" "/home/sunkehui/workspace/svn/project_RecordPlatform/source/sms-project/sms-project/sms-live/sms-server-core:home/sunkehui/workspace/svn/project_RecordPlatform/source/sms-project/sms-project/sms-rtsp:/lib/modules/4.4.0-139-generic/build")
+
+
+(setq my-jdb-sourcepath "/home/sunkehui/workspace/svn/project_RecordPlatform/source/sms-project/sms-project/sms-local-record/sms-local-boot/src/main/java:/home/sunkehui/workspace/svn/project_RecordPlatform/source/sms-project/sms-project/sms-live-httpflv/src:/home/sunkehui/workspace/svn/project_RecordPlatform/source/sms-project/sms-project/sms-live/sms-server-core/src")
+(setq my-jdb-break "com.seaeagletech.sms.plugin.httpflv.HttpFlvServlet.doHandler")
+
+(provide 'init-my)
