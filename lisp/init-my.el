8311d3c001a69025125b847a0b8174ec8d6c987b
Sun <skh7518671@gmail.com>
2019-05-27 09:11:43 +0800

    add select word and line

--- lisp/init-my.el
+++ lisp/init-my.el
@@ -1,6 +1,23 @@
 ;;TODO init ggtags mode
 
+(defun my-select-current-word ()
+  "Select the word under cursor.
+“word” here is considered any alphanumeric sequence with “_” or “-”."
+  (interactive)
+  (let (pt)
+    (skip-chars-backward "-_A-Za-z0-9")
+    (setq pt (point))
+    (skip-chars-forward "-_A-Za-z0-9")
+    (set-mark pt)))
 
+
+(defun my-select-current-line ()
+  "Select the current line"
+  (interactive)
+  (end-of-line) ; move to end of line
+  (set-mark (line-beginning-position)))
+
+(global-set-key (kbd "<f8>") 'my-select-current-word)
 
 (setenv "GTAGSLIBPATH" "/home/sunkehui/workspace/svn/project_RecordPlatform/source/sms-project/sms-project/sms-live/sms-server-core:home/sunkehui/workspace/svn/project_RecordPlatform/source/sms-project/sms-project/sms-rtsp:/lib/modules/4.4.0-139-generic/build")
 
