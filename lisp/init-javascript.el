4fd0c76c23aff52df3a433d5f259f86147cf0296
sunkehui112300 <skh7518671@gmail.com>
2019-07-15 08:35:21 +0800

    add js tern

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -54,12 +54,34 @@
 (when (and (executable-find "ag")
            (maybe-require-package 'xref-js2))
   (after-load 'js2-mode
+    ;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
+    ;; unbind it.
     (define-key js2-mode-map (kbd "M-.") nil)
     (add-hook 'js2-mode-hook
               (lambda () (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))))
 
 
 
+;;;  tern auto-company
+(when (and (executable-find "tern")
+           (maybe-require-package 'company-tern))
+  (after-load 'js2-mode
+    (require 'company)
+    (require 'company-tern)
+    (add-to-list 'company-backends 'company-tern)
+    (add-hook 'js2-mode-hook (lambda ()
+                               (tern-mode)
+                               (company-mode)))
+    ;; Disable completion keybindings, as we use xref-js2 instead
+    (define-key tern-mode-keymap (kbd "M-.") nil)
+    (define-key tern-mode-keymap (kbd "M-,") nil)
+    (setq company-tern-meta-as-single-line t)
+    (setq company-tooltip-align-annotations t)
+    )
+  )
+
+
+
 ;;; Coffeescript
 
 (after-load 'coffee-mode
